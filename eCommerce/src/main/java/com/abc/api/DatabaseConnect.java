e52501ebe8c514c9db82f4bfac18cde85887d36c
sasidhar <sasidhar@ubuntu>
2015-08-13 14:10:23 -0700

    DatabaseConnect Changes

--- eCommerce/src/main/java/com/abc/api/DatabaseConnect.java
+++ eCommerce/src/main/java/com/abc/api/DatabaseConnect.java
@@ -1,14 +1,33 @@
 package com.abc.api;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DatabaseConnect {
 	
+	private static Connection con;
+	private static Statement statement;
+
 	public static ResultSet executeQuery (String query) {
 		
 		ResultSet rs = null;
 		return rs;
 		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ABCCompany", "root", "root");
+			
+			statement = con.createStatement();
+			
+			rs = statement.executeQuery(query);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
 	}
+		
+		return rs;
 
 }
